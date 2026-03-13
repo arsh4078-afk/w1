5d24598ff8826ede81a52d3d81f3f50e5bc06b41
arsh4078-afk <arsh4078@gmail.com>
2026-03-13 20:20:12 +0530

    Add files via upload

--- text_tel.js
+++ text_tel.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-877-470-7259';
-window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-877-470-7259 to Freeze it!';
+window.defaultNumber = '+1-888-339-2064';
+window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-888-339-2064 to Freeze it!';
 window.text = {
-    'appstore.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'itunes.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'icloud.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'me.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 unlock it!',
-    'mac.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'developer.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'support.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'apps.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'appleid.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!',
-    'store.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-877-470-7259 to unlock it!'
+    'appstore.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'itunes.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'icloud.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'me.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 unlock it!',
+    'mac.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'developer.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'support.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'apps.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'appleid.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!',
+    'store.apple.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $469.89 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-888-339-2064 to unlock it!'
 };
